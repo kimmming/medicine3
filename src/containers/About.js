1af9e9b7ad1677f6e2a54b437b68a30258ccd904
heyryu96 <heyryu96@gmail.com>
2020-12-16 17:07:23 +0900

    about 페이지 수정

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import about_pic from './about2.jpeg';
 
 const About = () => {
     return(
         <div>
-            소개
+            <center>
+                <div>
+               <img src={about_pic}
+               width='700'
+               height='700'
+               />
+               </div>
+            </center>
         </div>
     );
 };
